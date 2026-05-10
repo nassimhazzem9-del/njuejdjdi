3f134d6829283f4a774e1d83d58a4da0634d95e1
nassimhazzem9-del <nassimhazzem9@gmail.com>
2026-05-10 17:27:35 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-setInterval(()=>{
-fetch('/status')
-.then(res=>res.json())
-.then(data=>{
-document.getElementById("cpu").innerText = "CPU: " + data.cpu + "%";
-document.getElementById("ram").innerText = "RAM: " + data.ram + "%";
-});
-},2000);
